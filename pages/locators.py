3dbf7e1eb895b87c4b8275cf9f6e53d4cfab0bfc
GERAunits <gerasape@gmail.com>
2020-05-24 13:04:21 +0300

    Реализация should_be_login_url should_be_login_form should_be_register_form

--- pages/locators.py
+++ pages/locators.py
@@ -4,3 +4,9 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
+class LoginPageLocators(object):
+    LOGIN_URL_KEY = 'login'
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -9,12 +10,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert LoginPageLocators.LOGIN_URL_KEY in self.browser.current_url, "Login not presented in url"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form not presented"
