284590c002048e4d7ab6e83b78ef6558dbdfe28a
GERAunits <gerasape@gmail.com>
2020-05-24 11:54:40 +0300

    Добавляем паттерн BasePage

--- conftest.py
+++ conftest.py
@@ -5,8 +5,9 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default='None',
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language: ru, en, es, fr ... (etc.)")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
@@ -27,33 +28,3 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-#
-#
-# def pytest_addoption(parser):
-#     parser.addoption('--language', action='store', default=None,
-#                      help="Choose language: es or fr")
-#
-#
-# @pytest.fixture(scope="function")
-# def browser(request):
-#     language = request.config.getoption("language")
-#     browser = None
-#     if language == "es":
-#         print("\nstart test language es..")
-#         options = Options()
-#         options.add_experimental_option('prefs', {'intl.accept_languages': 'es'})    # ru en
-#         browser = webdriver.Chrome(options=options)
-#     elif language == "fr":
-#         print("\nstart test language fr..")
-#         options = Options()
-#         options.add_experimental_option('prefs', {'intl.accept_languages': 'fr'})    # ru en
-#         browser = webdriver.Chrome(options=options)
-#     else:
-#         print("\n--language should be es or fr")
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
--- test_main_page.py
+++ test_main_page.py
@@ -12,3 +12,12 @@ def go_to_login_page(browser):
 def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     go_to_login_page(browser)
+
+
+# http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/
+# def test_add_to_cart(browser):
+#     page = ProductPage(url="", browser)   # инициализируем объект Page Object
+#     page.open()                           # открываем страницу в браузере
+#     page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
+#     page.add_product_to_cart()            # жмем кнопку добавить в корзину
+#     page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
