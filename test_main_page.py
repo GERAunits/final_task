1cef9f5eb87bbb90ae2b49b9eeb371cb04c9a5ba
GERAunits <gerasape@gmail.com>
2020-05-24 12:00:48 +0300

    Переписали test_main_page с помощью Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,8 @@
-from time import sleep
-# import time
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
-
-
-# http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/
-# def test_add_to_cart(browser):
-#     page = ProductPage(url="", browser)   # инициализируем объект Page Object
-#     page.open()                           # открываем страницу в браузере
-#     page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
-#     page.add_product_to_cart()            # жмем кнопку добавить в корзину
-#     page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
