0a7b44fb7e1ed9337b0e7e3568208569705c2642
GERAunits <gerasape@gmail.com>
2020-05-25 14:45:37 +0300

    Тест login_guest через класс TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,26 @@
 import pytest
 from .pages.main_page import MainPage
 from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+
+
+MAIN_PAGE_LINK = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, MAIN_PAGE_LINK)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, MAIN_PAGE_LINK)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_should_see_login_link(browser):
